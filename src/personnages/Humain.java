8e42e593b86f7654f54b75ee7d1466f6f3e237fc
moana <moana@DESKTOP-JBR1VMA.private.univ-tlse3.fr>
2024-11-07 15:03:38 +0100

    tp1 java

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,19 +1,20 @@
-
+package personnages;
 
 public class Humain {
-	
+
 	public String nom;
 	public int argent;
 	public String boisson;
-	
+		
 	public Humain(String nom, int argent, String boisson) {
 		this.nom = nom;
 		this.argent = argent;
 		this.boisson = boisson;
 	}
-	
-	public void parler(String texte) {
 		
+	public void parler(String texte) {
+
 	}
+
 
 }
